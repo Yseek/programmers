9b527d7edc163e1e6177599ba908e110514ae565
Yseek <yun5003@naver.com>
2025-06-23 09:56:22 +0900

    :sparkles: x만큼 간격이 있는 n개의 숫자

--- src/test/java/com/codingtest/test/solution/SolutionTest.java
+++ src/test/java/com/codingtest/test/solution/SolutionTest.java
@@ -2,6 +2,7 @@ package com.codingtest.test.solution;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 class SolutionTest {
@@ -61,5 +62,28 @@ class SolutionTest {
         int answer = solution.test5(n);
         //then
         assertThat(answer).isEqualTo(6);
+    }
+
+    @Test
+    @DisplayName("자연수 뒤집어 배열로 만들기")
+    void test6() {
+        //given
+        long n = 12345;
+        //when
+        int[] answer = solution.test6(n);
+        //then
+        assertThat(answer).containsExactly(5, 4, 3, 2, 1);
+    }
+
+    @Test
+    @DisplayName("x만큼 간격이 있는 n개의 숫자")
+    void test7() {
+        //given
+        int x = 2;
+        int n = 5;
+        //when
+        long[] answer = solution.test7(x, n);
+        //then
+        assertThat(answer).containsExactly(2, 4, 6, 8, 10);
     }
 }
